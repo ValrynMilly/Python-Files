271eb4d9dab977f95efc5941ca317c5b091a5418
Emiljano <Emiljanokurbiba@gmail.com>
2020-06-02 09:48:45 +0100

    Update 01June.py

--- Challenges of the Day/01June.py
+++ Challenges of the Day/01June.py
@@ -1,4 +1,7 @@
-numbers = range[2000, 3001]
+numbers = range(2000, 3001)
 for i in numbers:
-    if i % 7==0 & i *5 !=:
-        numbers + i,
+    if i % 7==0:
+        numbers.append(i)
+        if i *5!=0:
+            numbers.append(i)
+    print(numbers)
