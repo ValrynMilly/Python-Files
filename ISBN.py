e28809eaaea716ce0709078d45fe433238eba44a
Emiljano <Emiljanokurbiba@gmail.com>
2020-06-02 11:57:45 +0100

    ISBN now complete

--- ISBN.py
+++ ISBN.py
@@ -1,4 +1,13 @@
 numbers = input("please enter your 13 Digit ISBN number: ")
+digits = [int(x) for x in str(numbers)]
+#print(digits)
+new_digits = []
 
-for i in numbers:
-    if i % 2
+for i in digits:
+    if int(i) % 2 != 0:
+        new_digits.append(i)
+    elif int(i) % 2 == 0:
+        i=i*3
+        new_digits.append(i)
+allsum = sum(new_digits) / 10
+print(allsum)
