058e40c632405205440fcb85c92f97276150b716
Emiljano <Emiljanokurbiba@gmail.com>
2020-06-02 09:36:47 +0100

    Added first Python Files

---
+++ Challenges of the Day/01June.py
@@ -0,0 +1,4 @@
+numbers = range[2000, 3001]
+for i in numbers:
+    if i % 7==0 & i *5 !=:
+        numbers + i,
---
+++ ISBN.py
@@ -0,0 +1,4 @@
+numbers = input("please enter your 13 Digit ISBN number: ")
+
+for i in numbers:
+    if i % 2
